13756f1c2523346c8f3723fa037e4f7b8dc5e4bc
Ray Zane <raymondzane@gmail.com>
2024-10-17 19:58:57 -0400

    Extract to schema

--- spec/database_objects_spec.rb
+++ spec/database_objects_spec.rb
@@ -1,63 +1,42 @@
 # frozen_string_literal: true
 
 RSpec.describe DatabaseObjects do
+  around do |example|
+    ActiveRecord::Base.transaction do
+      example.run
+    ensure
+      raise ActiveRecord::Rollback
+    end
+  end
+
   it "has a version number" do
     expect(DatabaseObjects::VERSION).not_to be nil
   end
 
-  describe 'declare_view with SQL' do
-    let(:model) do
-      define_model do
-        declare_view 'SELECT generate_series(1, 1) AS index'
-      end
+  describe 'declare_view' do
+    it 'acts like a view with SQL' do
+      expect(SeriesView.all).to contain_exactly(
+        have_attributes(index: 1),
+        have_attributes(index: 2),
+        have_attributes(index: 3),
+      )
     end
 
-    it 'acts like a view' do
-      expect(model.all).to contain_exactly(have_attributes(index: 1))
-    end
-  end
-
-  describe 'declare_view with ActiveRecord' do
-    let(:model) do
-      define_model do
-        declare_view { Person.where(name: 'Gary') }
-      end
-    end
-
-    it 'acts like a view' do
+    it 'acts like a view with ActiveRecord' do
       Person.create!(name: 'Gary')
       Person.create!(name: 'Other')
 
-      expect(model.all).to contain_exactly(have_attributes(name: 'Gary'))
+      expect(GaryView.all).to contain_exactly(have_attributes(name: 'Gary'))
     end
   end
 
   describe 'declare_function' do
-    let(:model) do
-      define_model do
-        declare_function :generate_series
-      end
-    end
-
     it 'can be executed' do
-      expect(model.execute(1, 3)).to contain_exactly(
-        have_attributes(examples: 1),
-        have_attributes(examples: 2),
-        have_attributes(examples: 3)
+      expect(SeriesFunction.execute(1, 3)).to contain_exactly(
+        have_attributes(series_functions: 1),
+        have_attributes(series_functions: 2),
+        have_attributes(series_functions: 3)
       )
     end
-  end
-
-  def define_model(&)
-    model = Class.new(ActiveRecord::Base) do
-      extend DatabaseObjects::Schema
-      extend DatabaseObjects::View
-      extend DatabaseObjects::Function
-
-      self.table_name = 'examples'
-    end
-
-    model.class_eval(&)
-    model
   end
 end
--- spec/support/schema.rb
+++ spec/support/schema.rb
@@ -13,3 +13,28 @@ end
 
 class Person < ActiveRecord::Base
 end
+
+class GaryView < ActiveRecord::Base
+  extend DatabaseObjects::Schema
+  extend DatabaseObjects::View
+
+  declare_view do
+    Person.where(name: 'Gary')
+  end
+end
+
+class SeriesView < ActiveRecord::Base
+  extend DatabaseObjects::Schema
+  extend DatabaseObjects::View
+
+  declare_view <<~SQL
+    SELECT generate_series(1, 3) AS index
+  SQL
+end
+
+class SeriesFunction < ActiveRecord::Base
+  extend DatabaseObjects::Schema
+  extend DatabaseObjects::Function
+
+  declare_function :generate_series
+end
